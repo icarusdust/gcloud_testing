ee76db4d2e8bff799b24caedc2f443eab017c5a8
Nihal Aghazada <nihal@nevercode.io>
2022-04-18 14:46:11 +0300

    rrr

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,14 +16,13 @@
 # echo $GCLOUD_SERVICE_ACCOUNT_CREDENTIALS 
 # "{\"service_account\":\"value\"}"
 
+export GCLOUD_SERVICE_ACCOUNT_CREDENTIALS=`jq @json < service_account_test.json`
+
 curl -H "Content-Type: application/json" -H "x-auth-token: kjTAfwKBKgeHyPLOVqAd6AzkEycsuY-alno6Zq8Ezzo" \
 --data '{
-    "appId": "625beccddfd8ade9df248265", 
-    "workflowId": "android-workflow",
-    "branch": "master", 
     "environment": { 
         "variables": { 
-            "GCLOUD_CREDENTIALS": "$GCLOUD_SERVICE_ACCOUNT_CREDENTIALS"
+            "GCLOUD_CREDENTIALS": $GCLOUD_SERVICE_ACCOUNT_CREDENTIALS
         }
     }
 }' \
