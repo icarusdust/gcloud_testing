1c27c4cee0a42fd433c2a7af28faa2999929a418
Nihal Aghazada <nihal@nevercode.io>
2022-04-18 13:17:04 +0300

    dfd

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,26 @@
-FILE=$(awk 1 ORS='\\n' $CM_BUILD_DIR/gcloud_testing/android/creds.json)
+# FILE=$(awk 1 ORS='\\n' /android/creds.json)
+# curl -H "Content-Type: application/json" -H "x-auth-token: kjTAfwKBKgeHyPLOVqAd6AzkEycsuY-alno6Zq8Ezzo" \
+# --data '{
+#     "appId": "625beccddfd8ade9df248265", 
+#     "workflowId": "android-workflow",
+#     "branch": "master", 
+#     "environment": { 
+#         "variables": { 
+#             "GCLOUD_SERVICE_ACCOUNT_CREDENTIALS": "$FILE"
+#         }
+#     }
+# }' \
+# https://api.codemagic.io/builds
+
+
 curl -H "Content-Type: application/json" -H "x-auth-token: kjTAfwKBKgeHyPLOVqAd6AzkEycsuY-alno6Zq8Ezzo" \
 --data '{
-    "appId": "625a9cbf5d82902e63ca38bf", 
-    "workflowId": "ios-release",
+    "appId": "625beccddfd8ade9df248265", 
+    "workflowId": "android-workflow",
     "branch": "master", 
     "environment": { 
         "variables": { 
-            "APP_STORE_ID": "1565355012",
-            "GCLOUD_CREDENTIALS": "$FILE",
-            "BUNDLE_ID": "io.nevercode.flutterapp.dev",
-            "XCODE_SCHEME": "dev", 
-            "XCODE_CONFIG": "Release-dev",
-            "ENTRY_POINT": "lib/main_dev.dart"
+            "GCLOUD_CREDENTIALS": $GCLOUD_SERVICE_ACCOUNT_CREDENTIALS
         }
     }
 }' \
